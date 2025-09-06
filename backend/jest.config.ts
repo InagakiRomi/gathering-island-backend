26394d2bd8baf90c161f1de6415dbe15d920d62b
InagakiRomi <100176049+InagakiRomi@users.noreply.github.com>
2025-09-06 20:12:24 +0800

    [+]單元測試設定

---
+++ backend/jest.config.ts
@@ -0,0 +1,17 @@
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
+
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
+
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  rootDir: './',
+};
