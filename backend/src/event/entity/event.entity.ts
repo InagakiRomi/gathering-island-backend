a27c3be6814c73126eab805ca0489cf8bb63cb96
InagakiRomi <100176049+InagakiRomi@users.noreply.github.com>
2025-09-12 16:40:21 +0800

    [!]簡化event.entity

--- backend/src/event/entity/event.entity.ts
+++ backend/src/event/entity/event.entity.ts
@@ -9,11 +9,11 @@ export class Event {
     event_id: number;
 
     /** 活動標題 */
-    @Column({ type: 'varchar', length: 32 })
+    @Column({ length: 32 })
     event_name: string;
 
     /** 活動描述 */
-    @Column({ type: 'varchar', length: 512 })
+    @Column({ length: 512 })
     event_description: string;
 
     /** 活動類型 */
@@ -21,11 +21,11 @@ export class Event {
     event_type: EventType;
 
     /** 活動地點 */
-    @Column({ type: 'varchar', length: 128 })
+    @Column({ length: 128 })
     event_location: string;
 
     /** 封面圖片 URL */
-    @Column({ type: 'varchar', length: 512, nullable: true })
+    @Column({ length: 512, nullable: true })
     image_url: string;
 
     /** 活動人數上限 */
@@ -34,7 +34,7 @@ export class Event {
 
     /** 活動費用 */
     @Min(0)
-    @Column({ type: 'int', default: 0, unsigned: true })
+    @Column({ default: 0, unsigned: true })
     event_price: number;
 
     /** 主辦人 ID */
@@ -46,18 +46,18 @@ export class Event {
     is_ended: boolean;
 
     /** 活動開始時間 */
-    @Column({ type: 'timestamp' })
+    @Column()
     event_time: Date;
 
     /** 報名截止時間 */
-    @Column({ type: 'timestamp' })
+    @Column()
     registration_deadline: Date;
 
     /** 建立時間，自動填入目前時間 */
-    @Column({ type: 'timestamp', nullable: true })
+    @Column({ nullable: true })
     created_at: Date;
 
     /** 最後更新資料時間 */
-    @Column({ type: 'timestamp' })
+    @Column()
     updated_at: Date;
 }
