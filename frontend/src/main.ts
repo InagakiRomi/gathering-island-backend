7f5807f0ed5c7c4d87152e977352b8ae93627a08
InagakiRomi <100176049+InagakiRomi@users.noreply.github.com>
2025-12-01 17:41:29 +0800

    [-]移除舊前端資料

--- frontend/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './assets/styles/variables.css';
-
-const app = createApp(App)
-
-app.use(router)
-app.mount('#app')
